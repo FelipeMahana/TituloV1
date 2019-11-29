5f9870c2c7a41031ff0b52a5bb43fbfd9a592d08
Felipe <f.mahana94@gmail.com>
2019-11-29 01:55:34 -0300

    min/max resultados new

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,3 +1,11 @@
 class Result < ApplicationRecord
   belongs_to :user
+
+  #probando validacion
+  #validates_numericality_of :hundred_mts, greater_than_or_equal_to: 9.5, message: 'hola'
+  #validate :greater_than_or_equal_to
+
+  #def greater_than_or_equal_to
+  #  errors.add(:hundred_mts, 'ola')
+  #end
 end
